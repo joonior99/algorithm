83397222407eac1a2ec1d70009ae4f25179de72d
Joonyeon Hwang <hjyny@naver.com>
2021-09-18 17:34:05 +0900

    Create 6070.py

---
+++ CodeUp/조건선택실행구조/6070.py
@@ -0,0 +1,13 @@
+// 월 입력받아 계절 출력하기
+
+n = int(input())
+if n//3==1:
+  print('spring')
+else:
+  if n//3==2:
+    print('summer')
+  else:
+    if n//3==3:
+      print('fall')
+    else
+      print('winter')
