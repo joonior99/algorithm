25e2103ad4930d4971d836f35d07c82cf36c2a80
Joonyeon Hwang <hjyny@naver.com>
2021-09-22 12:10:22 +0900

    Update 6082.py

--- CodeUp/종합/6082.py
+++ CodeUp/종합/6082.py
@@ -4,8 +4,8 @@
 
 n = int(input())
 
-for i in range(1, n+1) :
-  if i%10==3 or i%10==6 or i%10==9 :
+for i in range(1, n+1):
+  if i%10==3 or i%10==6 or i%10==9:
     print("X", end=' ') // 출력 후 공백문자(빈칸, ' ')로 끝냄
-  else :
+  else:
     print(i, end=' ')
