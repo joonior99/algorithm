af98939e790534061643d5ea5c0a3653cbeeb6fc
Joonyeon Hwang <hjyny@naver.com>
2021-09-23 11:32:36 +0900

    Create 6089.py

---
+++ CodeUp/종합/6089.py
@@ -0,0 +1,14 @@
+// 수 나열하기 2
+
+// 시작 값(a), 등비(r), 몇 번째인지를 나타내는 정수(n)가 입력될 때 n번째 수를 출력하는 프로그램을 만들어보자
+
+a, r, n = map(int, input().split())
+print(a*r**(n-1))
+
+a, r, n = input().split()
+a = int(a)
+r = int(r)
+n = int(n)
+for i in range(1, n):
+  a = a*r
+print(a)
