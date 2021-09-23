f1812d4346854118ad5412139c6bc6ee35e47a1b
Joonyeon Hwang <hjyny@naver.com>
2021-09-23 15:05:25 +0900

    Create 6094.py

---
+++ CodeUp/리스트/6094.py
@@ -0,0 +1,17 @@
+// 이상한 출석 번호 부르기 3
+
+// 출석 번호를 n번 무작위로 불렀을 때, 가장 빠른 번호를 출력해 보자
+// 단, 첫 번째 번호와 마지막 번호가 몇 번인지는 아무도 모르고, 음수(-) 번호, 0번 번호도 있을 수 있다
+
+n = int(input()) // 10
+a = input().split() // 10 4 2 3 6 6 7 9 8 5
+
+for i in range(n):
+  a[i] = int(a[i])
+
+min = a[0]
+for i in range(0, n):
+  if a[i] < min:
+    min = a[i]
+
+print(min)
