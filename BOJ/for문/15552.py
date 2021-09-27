d0f3e071a0d801c120bdb8f8264fd399895b8a95
Joonyeon Hwang <hjyny@naver.com>
2021-09-27 23:24:08 +0900

    Create 15552.py

---
+++ BOJ/for문/15552.py
@@ -0,0 +1,11 @@
+// 빠른 A+B
+
+// 본격적으로 for문 문제를 풀기 전에 주의해야 할 점이 있다
+// 입출력 방식이 느리면 여러 줄을 입력받거나 출력할 때 시간초과가 날 수 있다는 점이다
+// 첫 줄에 테스트케이스의 개수 T가 주어지고, 다음 T줄에는 각각 두 정수 A와 B가 주어진다
+
+import sys // sys 모듈을 불러오면 stdin.readline() 함수를 사용할 수 있다
+t = int(sys.stdin.readline())
+for _ in range(1, n+1):
+  a, b = map(int, sys.stdin.readline().split())
+  print(a+b)
