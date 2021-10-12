78f780ae29c6ec8f9e9db627e0fcf74249a796ff
Joonyeon Hwang <hjyny@naver.com>
2021-10-12 16:45:19 +0900

    Update 11653.py

--- BOJ/기본수학2/11653.py
+++ BOJ/기본수학2/11653.py
@@ -8,7 +8,7 @@ if n == 1:
   print('')
 
 for i in range(2, n+1): // 2부터 하나씩 나눠보기
-  if n%i == 0: // 해당 숫자로 나눌 수 없을 때까지 나누기
-    while n%i == 0:
+  if n%i == 0: // 숫자가 나눠질 수 있는지 
+    while n%i == 0: // 해당 숫자로 나눌 수 없을 때까지 나누기
       print(i)
       n = n/i
